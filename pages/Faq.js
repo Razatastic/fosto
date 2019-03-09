e5648ba0ca8ab05da3d96c20730572146889940a
Abdal <abdal720@gmail.com>
2019-03-09 17:21:19 -0500

    Created JSON file to store Q+A, updated FAQ file

--- pages/Faq.js
+++ pages/Faq.js
@@ -1,40 +1,22 @@
+import "../styles/main.scss";
+var data = require("../database/faq.json");
 
-
-// created a functional component for frequent asked questions 
-function Faq(){
-    return(
-        <div> <h2 className="vertical-center"> This is FAQ Page</h2>
-        <h5> 1. Whats's the main purpose of Fosto?</h5>
-        <h6>    	■ Fosto is designed in an effort to help solve the problem of not having 
-            a universal lost and found application for the general community.  </h6>
-
-            <h5> 2. How does it work?</h5>
-        <h6>    	■ Users of Fosto are given two options when they first enter the website. A Lost and Found section. </h6>
-
-          <h6>      ■ They can either click on the lost section to describe an item they found, or click on a found item to connect 
-                        to the person that lost it, and create a listing on the map that would show up as a pin-drop.</h6> 
-
-            <h5> 3. How long does a user created listing last?</h5>
-            <h6>    	■ It lasts for 10 days. Users have the option to renew their listing if they want an extension. </h6>
-
-            <h5> 4. What are some ways a user can communicate with the person that lost or found an item ?</h5>
-            <h6>    	■ They can use a Live chat system or an email to communicate with another user. </h6>
-
-            <h5> 5. When happens when a user finds an item ?</h5>
-            <h6>    	■ They can drop off at a closeby institution or a government agency, and create a listing on the map. </h6>
-
-            <h5> 6. When happens when a user loses an item ?</h5>
-            <h6>    	■ They can create a listing on the map to ask other users to help find it. 
-                In return they can provide some sort of a reward. </h6>
-
-
-
-
-            
-            
-        </div>
-    )
+// created a functional component for frequent asked questions
+function FAQ() {
+  return (
+    <div>
+      {/* Title */}
+      <h2 className="vertical-center">Frequently Asked Questions</h2>
+      <ol>
+        {data.map(dataValue => (
+          <li>
+            <h5> {dataValue.q}</h5>
+            <h6> {dataValue.a}</h6>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
-
-export default Faq
+export default FAQ;
