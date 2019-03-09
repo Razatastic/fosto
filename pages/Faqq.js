d42bae5af8b58b5f5770fd5e3d013d3b45d2fc8f
Ahmad Raza <raza2157@gmail.com>
2019-03-09 18:04:24 -0500

    rename

--- pages/Faqq.js
+++ pages/Faqq.js
@@ -1,10 +1,12 @@
 import "../styles/main.scss";
+import "../components/Navbar";
 var data = require("../database/faq.json");
 
 // created a functional component for frequent asked questions
 function FAQ() {
   return (
     <div>
+      <Navbar />
       {/* Title */}
       <h2 className="vertical-center">Frequently Asked Questions</h2>
       <ol>
