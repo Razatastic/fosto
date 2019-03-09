4a8d44e1dca4cd1dafa80c5f0111c656a5970831
Abdal <abdal720@gmail.com>
2019-03-09 16:17:30 -0500

    Wrote the FAQ section
    Answered a couple of basic user questions on the FAQ.js page.

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,18 +2,31 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
+
+    <div>
     <ul className="nav">
       <li className="homeTab">
         <Link href="/index">
           <a>Fosto</a>
         </Link>
       </li>
+  
+  <li
+   className = "homeTab">
+   <Link href="/Faq"> 
+   <a> FAQ</a>
    </Link>
+  </li>
+  
       <li className="loginTab">
         <Link href="/authentication/login">
           <a>Login</a>
         </Link>
       </li>
+
+    
     </ul>
+  
+    </div>
   );
 }
--- pages/Faq.js
+++ pages/Faq.js
@@ -4,8 +4,34 @@
 function Faq(){
     return(
         <div> <h2 className="vertical-center"> This is FAQ Page</h2>
-        <h5> Whats's the main purpose of Fosto?</h5>
-        <h6> Fosto is designed in an effort to help solve the problem of not having a general lost and found application for the general community.  </h6>
+        <h5> 1. Whats's the main purpose of Fosto?</h5>
+        <h6>    	■ Fosto is designed in an effort to help solve the problem of not having 
+            a universal lost and found application for the general community.  </h6>
+
+            <h5> 2. How does it work?</h5>
+        <h6>    	■ Users of Fosto are given two options when they first enter the website. A Lost and Found section. </h6>
+
+          <h6>      ■ They can either click on the lost section to describe an item they found, or click on a found item to connect 
+                        to the person that lost it, and create a listing on the map that would show up as a pin-drop.</h6> 
+
+            <h5> 3. How long does a user created listing last?</h5>
+            <h6>    	■ It lasts for 10 days. Users have the option to renew their listing if they want an extension. </h6>
+
+            <h5> 4. What are some ways a user can communicate with the person that lost or found an item ?</h5>
+            <h6>    	■ They can use a Live chat system or an email to communicate with another user. </h6>
+
+            <h5> 5. When happens when a user finds an item ?</h5>
+            <h6>    	■ They can drop off at a closeby institution or a government agency, and create a listing on the map. </h6>
+
+            <h5> 6. When happens when a user loses an item ?</h5>
+            <h6>    	■ They can create a listing on the map to ask other users to help find it. 
+                In return they can provide some sort of a reward. </h6>
+
+
+
+
+            
+            
         </div>
     )
 }
