8bb968ec5ccacee48178893df28eec7c0aa5dad3
Ahmad Raza <raza2157@gmail.com>
2019-03-09 18:10:01 -0500

    Updated styling for FAQ page

--- pages/FAQ.js
+++ pages/FAQ.js
@@ -1,5 +1,5 @@
 import "../styles/main.scss";
-import "../components/Navbar";
+import Navbar from "../components/Navbar";
 var data = require("../database/faq.json");
 
 // created a functional component for frequent asked questions
@@ -7,16 +7,18 @@ function FAQ() {
   return (
     <div>
       <Navbar />
-      {/* Title */}
-      <h2 className="vertical-center">Frequently Asked Questions</h2>
-      <ol>
-        {data.map((dataValue, i) => (
-          <li key={i}>
-            <h5> {dataValue.q}</h5>
-            <h6> {dataValue.a}</h6>
-          </li>
-        ))}
-      </ol>
+      <div className="container">
+        {/* Title */}
+        <h2 className="vertical-center">Frequently Asked Questions</h2>
+        <ul>
+          {data.map((dataValue, i) => (
+            <li key={i}>
+              <p className="questionStyle"> {dataValue.q}</p>
+              <p className="answerStyle"> {dataValue.a}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
