0ac0e0b2789baca03de724bf757d99b663ec2ac0
Abdal <abdal720@gmail.com>
2019-03-10 19:11:27 -0400

    created FaqIcon.js component
    it shows the icon next to Frequently Asked Questions

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "../styles/Faq.css";
+import FaqIcon from "../components/FaqIcon";
+import { Icon } from "antd";
+
 var data = require("../database/faq.json");
 
 // created a functional component for frequent asked questions
@@ -7,7 +10,9 @@ function Faq() {
   return (
     <div>
       {/* Title */}
-      <h2 className="vertical-center faqTitle">Frequently Asked Questions</h2>
+      <h2 className="vertical-center faqTitle">
+        Frequently Asked Questions - <FaqIcon className="faq-title-Icon" />
+      </h2>
       <ol>
         {data.map((dataValue, i) => (
           <li key={i}>
