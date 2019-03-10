27487b1c87bb3d64cc3b9de68dcbeb291ce65f31
Kenneth Dorji <kennethdorji@gmail.com>
2019-03-10 17:59:37 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/components/LostForm.js
+++ src/components/LostForm.js
@@ -1,5 +1,16 @@
-import React, { Component } from 'react';
-import { Form, DatePicker, Button } from 'antd';
+import React, { Component } from "react";
+import {
+  Form,
+  DatePicker,
+  Button,
+  Input,
+  Tooltip,
+  Select,
+  AutoComplete
+} from "antd";
+
+const { Option } = Select;
+const AutoCompleteOption = AutoComplete.Option;
 
 class LostForm extends Component {
   handleSubmit = e => {
@@ -13,14 +24,64 @@ class LostForm extends Component {
       // Should format date value before submit.
       const values = {
         ...fieldsValue,
-        'date-picker': fieldsValue['date-picker'].format('YYYY-MM-DD')
+        "date-picker": fieldsValue["date-picker"].format("YYYY-MM-DD")
       };
-      console.log('Received values of form: ', values);
+      console.log("Received values of form: ", values);
     });
+  };
+
+  state = {
+    confirmDirty: false,
+    autoCompleteResult: []
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  handleConfirmBlur = e => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
+
+  compareToFirstPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue("password")) {
+      callback("Two passwords that you enter is inconsistent!");
+    } else {
+      callback();
+    }
+  };
+
+  validateToNextPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(["confirm"], { force: true });
+    }
+    callback();
+  };
+
+  handleWebsiteChange = value => {
+    let autoCompleteResult;
+    if (!value) {
+      autoCompleteResult = [];
+    } else {
+      autoCompleteResult = [".com", ".org", ".net"].map(
+        domain => `${value}${domain}`
+      );
+    }
+    this.setState({ autoCompleteResult });
   };
 
   render() {
     const { getFieldDecorator } = this.props.form;
+    const { autoCompleteResult } = this.state;
+
     const formItemLayout = {
       labelCol: {
         xs: { span: 24 },
@@ -33,14 +94,84 @@ class LostForm extends Component {
     };
     const config = {
       rules: [
-        { type: 'object', required: true, message: 'Please select a date!' }
+        { type: "object", required: true, message: "Please select a date!" }
       ]
     };
 
+    const prefixSelector = getFieldDecorator("prefix", {
+      initialValue: "+1"
+    })(
+      <Select style={{ width: 70 }}>
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    );
+
     return (
       <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-        <Form.Item label='DatePicker'>
-          {getFieldDecorator('date-picker', config)(<DatePicker />)}
+        <Form.Item label="Date">
+          {getFieldDecorator("date-picker", config)(<DatePicker />)}
+        </Form.Item>
+
+        <Form.Item
+          label={
+            <span>
+              Lost Item&nbsp;
+              <Tooltip title="What do you want others to call you?" />
+            </span>
+          }
+        >
+          {getFieldDecorator("itemLost", {
+            rules: [
+              {
+                required: true,
+                message: "Please input the item you've lost",
+                whitespace: true
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+
+        <Form.Item
+          label={
+            <span>
+              Location&nbsp;
+              <Tooltip title="What do you want others to call you?" />
+            </span>
+          }
+        >
+          {getFieldDecorator("location", {
+            rules: [
+              {
+                required: true,
+                message: "Please input the item you've lost",
+                whitespace: true
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+
+        <Form.Item label="E-mail">
+          {getFieldDecorator("email", {
+            rules: [
+              {
+                type: "email",
+                message: "The input is not valid E-mail!"
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!"
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+
+        <Form.Item label="Phone Number">
+          {getFieldDecorator("phone", {
+            rules: [
+              { required: false, message: "Please input your phone number!" }
+            ]
+          })(<Input addonBefore={prefixSelector} style={{ width: "100%" }} />)}
         </Form.Item>
 
         <Form.Item
@@ -49,7 +180,7 @@ class LostForm extends Component {
             sm: { span: 16, offset: 8 }
           }}
         >
-          <Button type='primary' htmlType='submit'>
+          <Button type="primary" htmlType="submit">
             Submit
           </Button>
         </Form.Item>
@@ -58,4 +189,4 @@ class LostForm extends Component {
   }
 }
 
-export default Form.create({ name: 'time_related_controls' })(LostForm);
+export default Form.create({ name: "time_related_controls" })(LostForm);
