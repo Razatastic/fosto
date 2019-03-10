86ab7f198302a92368ae640c8be0c39e2ffaefa8
Ahmad Raza <raza2157@gmail.com>
2019-03-10 17:58:01 -0400

    Updated Login page, removed unnecessary files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { Menu, Icon } from 'antd';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { Menu, Icon } from "antd";
+import { NavLink } from "react-router-dom";
 
 export default class App extends Component {
   state = {
-    current: 'home'
+    current: "home"
   };
 
   handleClick = e => {
@@ -18,33 +18,37 @@ export default class App extends Component {
       <Menu
         onClick={this.handleClick}
         selectedKeys={[this.state.current]}
-        mode='horizontal'
+        mode="horizontal"
       >
         {/* Link to Home Page */}
-        <Menu.Item key='home'>
-          <NavLink to='/'>
-            <Icon type='home' />
+        <Menu.Item key="home">
+          <NavLink to="/">
+            <Icon type="home" />
             Fosto
           </NavLink>
         </Menu.Item>
         {/* Link to Lost/Found Forms */}
-        <Menu.Item key='forms'>
-          <NavLink to='/forms'>
-            <Icon type='form' />
+        <Menu.Item key="forms">
+          <NavLink to="/forms">
+            <Icon type="form" />
             Forms
           </NavLink>
         </Menu.Item>
         {/* Link to FAQ Page */}
-        <Menu.Item key='faq'>
-          <NavLink to='/faq'>
-            <Icon type='question-circle' />
+        <Menu.Item key="faq">
+          <NavLink to="/faq">
+            <Icon type="question-circle" />
             FAQ
           </NavLink>
         </Menu.Item>
         {/* Link to Login Page */}
-        <Menu.Item key='login' onClick={this.handleBrick}>
-          <NavLink to='/login'>
-            <Icon type='login' />
+        <Menu.Item
+          key="login"
+          onClick={this.handleBrick}
+          style={{ float: "right" }}
+        >
+          <NavLink to="/login">
+            <Icon type="login" />
             Login
           </NavLink>
         </Menu.Item>
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,6 +1,93 @@
 import React from "react";
-import LoginForm from "../../components/auth/LoginForm";
+import { Row, Col, Form, Icon, Input, Button, Checkbox } from "antd";
+import { Link, withRouter } from "react-router-dom";
+import "../../styles/main.css";
 
-export default function Login() {
-  return <LoginForm />;
+// import { auth } from "../../components/Firebase/firebase";
+
+// const error = () => {
+//   message.error("This is a message of error");
+// };
+
+class NormalLoginForm extends React.Component {
+  // handleSubmit = e => {
+  //   e.preventDefault();
+  //   this.props.form.validateFields((err, values) => {
+  //     auth
+  //       .signInWithEmailAndPassword(values.email, values.password)
+  //       .then(result => {
+  //         const user = result.user;
+  //         this.props.history.push("/");
+  //         this.setState({
+  //           user
+  //         });
+  //       });
+  //   });
+  // };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Row className="center">
+        <Col span={6}>
+          <h2>Login</h2>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              {getFieldDecorator("email", {
+                rules: [{ required: true, message: "Please input your email!" }]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  placeholder="E-mail"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator("password", {
+                rules: [
+                  { required: true, message: "Please input your Password!" }
+                ]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  type="password"
+                  placeholder="Password"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator("remember", {
+                valuePropName: "checked",
+                initialValue: true
+              })(<Checkbox>Remember me</Checkbox>)}
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <a className="login-form-forgot" href=".">
+                Forgot password
+              </a>
+              <span>{" or"}</span>
+              <Link to="/register"> Register now!</Link>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    );
+  }
 }
+
+export default withRouter(
+  Form.create({ name: "normal_login" })(NormalLoginForm)
+);
