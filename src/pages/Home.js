d259001657f93185da56f001b76ee9c12de6955d
Abdal <abdal720@gmail.com>
2019-03-10 20:24:36 -0400

    Found & loaded Google Maps API
    loaded google maps api source code into Home.js file. 
    Received an api key from Google Cloud Services and moved it into .env file.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Layout } from "antd";
 
-export default function Home() {
-  return <h1>Hello</h1>;
+const { Content, Sider } = Layout;
+const mapApiKey = process.env.REACT_APP_GM_API_KEY;
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Layout>
+        <Sider>Sidebar goes here</Sider>
+        <Content>
+          <Map google={this.props.google} zoom={14}>
+            <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+            <InfoWindow onClose={this.onInfoWindowClose} />
+          </Map>
+        </Content>
+      </Layout>
+    );
+  }
 }
+
+export default GoogleApiWrapper({
+  apiKey: mapApiKey
+})(MapContainer);
