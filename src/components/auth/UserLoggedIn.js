f45381da84427bf52d92ee6bc49ecf092eff4118
Kenneth Dorji <kennethdorji@gmail.com>
2019-03-16 22:06:49 -0400

    Added the link path for register on  Login Page

--- src/components/auth/UserLoggedIn.js
+++ src/components/auth/UserLoggedIn.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "../Navbar";
 import Login from "../../pages/auth/Login";
+import Register from "../../pages/auth/Register";
 import Home from "../../pages/Home";
 import FAQ from "../../pages/Faq";
 import SubmissionForm from "../../pages/Submit";
@@ -15,7 +16,7 @@ export default function UserLoggedIn() {
         <Route path="/submission-form" component={SubmissionForm} />
         <Route path="/faq" component={FAQ} />
         <Route path="/login" component={Login} />
-        {/* <Route path="/register" component={Register} /> */}
+        <Route path="/register" component={Register} />
       </div>
     </BrowserRouter>
   );
