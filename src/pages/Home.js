e44abfb60e9588bb9a6899e74316188a25bfa1d7
Ahmad Raza <raza2157@gmail.com>
2019-04-24 18:01:56 -0400

    Styled Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <h1
+      style={{ height: "89vh", fontSize: "600%" }}
+      className="vertical-center"
+    >
+      Never "lose" anything again.
+    </h1>
+  );
+}
