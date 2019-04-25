1c3a08294ee854bf3664c187e088495c8ae63891
Ahmad Raza <raza2157@gmail.com>
2019-04-24 22:56:50 -0400

    Replaced hardcoded info in FAQ page
    Now populates from Firebase

---
+++ src/pages/Faq.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Container } from "reactstrap";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import LoadingIcon from "../components/layout/LoadingIcon";
+
+function Faq({ faq }) {
+  if (faq) {
+    return (
+      <Container style={{ marginTop: "5vh" }}>
+        {/* Title */}
+        <h1 className="faqTitle display-5">F.A.Q.</h1>
+        <ul style={{ listStyleType: "none" }}>
+          {faq &&
+            faq.map((dataValue, i) => (
+              <li key={i}>
+                <p className="questionStyle"> {dataValue.question}</p>
+                <p className="answerStyle"> {dataValue.answer}</p>
+              </li>
+            ))}
+        </ul>
+      </Container>
+    );
+  } else {
+    return <LoadingIcon />;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    faq: state.firestore.ordered.faq
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "faq" }])
+)(Faq);
