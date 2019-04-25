51d40fb3ce4aa7b4460ed59be02d8966de0b2246
Ahmad Raza <raza2157@gmail.com>
2019-04-24 22:55:45 -0400

    Began work on Dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,38 @@
 import React from "react";
-import Sidebar from "./Sidebar";
+import { Container, Row, Col } from "reactstrap";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import { Redirect } from "react-router-dom";
+import { PostButton } from "./";
 
-export default function Dashboard() {
+const Dashboard = ({ statuses, auth, notifications }) => {
+  if (!auth.uid) return <Redirect to="/signin" />;
   return (
-    <div>
-      <Sidebar />
-    </div>
+    <Container>
+      <Row>
+        <PostButton />
+      </Row>
+      <Row>
+        <Col sm="12" md="7">
+          {/* <ItemsList /> */}
+        </Col>
+        <Col sm="12" md={{ size: 4, offset: 1 }}>
+          {/* <MapView /> */}
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
+
+const mapStateToProps = state => {
+  return {
+    faq: state.firestore.statuses,
+    auth: state.firebase.auth
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "faq" }])
+)(Dashboard);
