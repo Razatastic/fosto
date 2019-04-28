5fc6c181504e1e36f7c632e30217e742d931ab10
Ahmad Raza <razatastic@gmail.com>
2019-04-28 03:50:54 -0400

    Removed Sidebar.js

--- src/components/dashboard/Sidebar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Listing from "./Listing";
-
-// leave sider width to 390. It breaks the listing if you decrease the width any further.
-function Sidebar() {
-  return <div>d</div>;
-}
-
-export default Sidebar;
