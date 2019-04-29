6a5f001696d2ded306fe3b9a5a98e31f7465f5d3
Ahmad Raza <razatastic@gmail.com>
2019-04-28 22:01:32 -0400

    Fixed API issue

--- src/components/dashboard/MapView.js
+++ src/components/dashboard/MapView.js
@@ -1,37 +1,33 @@
 import React from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-
-const mapApiKey = process.env.REACT_APP_GM_API_KEY;
+import { GoogleMap, Marker } from "@react-google-maps/api";
 
 function MapView({ currentLat, currentLng }) {
   if (currentLat == null || currentLng == null) {
     return <div />;
   }
   return (
-    <LoadScript id="script-loader" googleMapsApiKey={mapApiKey}>
-      <GoogleMap
-        id="circle-example"
-        mapContainerStyle={{
-          height: "600px",
-          width: "1000px"
+    <GoogleMap
+      id="circle-example"
+      mapContainerStyle={{
+        height: "600px",
+        width: "1000px"
+      }}
+      zoom={17}
+      center={{
+        lat: currentLat,
+        lng: currentLng
+      }}
+    >
+      <Marker
+        onLoad={marker => {
+          console.log("marker: ", marker);
         }}
-        zoom={17}
-        center={{
+        position={{
           lat: currentLat,
           lng: currentLng
         }}
-      >
-        <Marker
-          onLoad={marker => {
-            console.log("marker: ", marker);
-          }}
-          position={{
-            lat: currentLat,
-            lng: currentLng
-          }}
-        />
-      </GoogleMap>
-    </LoadScript>
+      />
+    </GoogleMap>
   );
 }
 
