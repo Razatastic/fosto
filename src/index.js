1b8c48a2eb5c94b958ba025bc7ab60543eac1963
Ahmad Raza <razatastic@gmail.com>
2019-04-30 14:27:21 -0400

    Added comments

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const store = createStore(
       userProfile: "users", // Tells the previous property where to get users from
       attachAuthIsReady: true
     }),
+    // Support for Redux dev-tools
     window.__REDUX_DEVTOOLS_EXTENSION__
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : f => f
